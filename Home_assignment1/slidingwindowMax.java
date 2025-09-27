349574fd1b4752eda9732388ef8c3408cfb9a926
sanskaryo <sanskartodwal@gmail.com>
2025-09-27 12:41:38 +0530

    minor changes

---
+++ Home_assignment1/slidingwindowMax.java
@@ -0,0 +1,20 @@
+
+
+public class slidingwindowMax{
+    
+    public static void main(String[] args) {
+     
+
+        int[] arr = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+
+        int[] result = maxSlidingWindow(arr, k);
+        System.out.println(Arrays.toString(result));
+      
+    }
+
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+
+    }
+    
+}
