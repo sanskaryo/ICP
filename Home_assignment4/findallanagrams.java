a87f0c583175eaa76a316d1240623696fccc2c2a
sanskaryo <sanskartodwal@gmail.com>
2025-09-30 23:51:48 +0530

    minor additions ignored .class files

---
+++ Home_assignment4/findallanagrams.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class findallanagrams {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        System.out.println(s1.length());
+        System.out.println(s2.length());
+    }
+    
+}
