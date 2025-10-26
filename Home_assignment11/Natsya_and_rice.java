1ae403a5d15486862fe6290eb2d3183b072e3971
sanskaryo <sanskartodwal@gmail.com>
2025-10-26 20:40:10 +0530

    added solutions to assignment 11

---
+++ Home_assignment11/Natsya_and_rice.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Natsya_and_rice {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0) {
+            int n = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+
+            int minTotal = n * (a - b);
+            int maxTotal = n * (a + b);
+            int minPack = c - d;
+            int maxPack = c + d;
+
+            if (maxTotal < minPack || minTotal > maxPack)
+                System.out.println("No");
+            else
+                System.out.println("Yes");
+        }
+    }
+
+
+    
+}
